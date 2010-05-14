ebb0fb80a1806b92d72d979643a6df493e58d7fa
dbetz <>
2010-05-13 20:40:17 -0500

    Add

---
+++ Client/Nalarium.Client.Mvc/UserControl.cs
@@ -0,0 +1,42 @@
+﻿#region Copyright
+//+ Nalarium Pro 3.0 - Client Module
+//+ Copyright © Jampad Technology, Inc. 2008-2010
+//+
+//+ This file is a part of the Nalarium Suite.
+//+ The use and distribution terms for this software are covered by the
+//+ Microsoft Permissive License (Ms-PL) which can be found at
+//+ http://www.microsoft.com/opensource/licenses.mspx.
+#endregion
+using System;
+//+
+namespace Nalarium.Client.Mvc
+{
+    public abstract class ViewUserControl<TModel> : System.Web.Mvc.ViewUserControl<TModel> where TModel : class
+    {
+        //- #Client -//
+        protected ClientHelper Client { get; set; }
+
+        //+
+        //- #OnInit -//
+        protected override void OnInit(EventArgs e)
+        {
+            Client = new ClientHelper((ViewContext.Controller as IControllerContainer).ClientController);
+            //+
+            base.OnInit(e);
+        }
+    }   
+    public abstract class ViewUserControl : System.Web.Mvc.ViewUserControl
+    {
+        //- #Client -//
+        protected ClientHelper Client { get; set; }
+
+        //+
+        //- #OnInit -//
+        protected override void OnInit(EventArgs e)
+        {
+            Client = new ClientHelper((ViewContext.Controller as IControllerContainer).ClientController);
+            //+
+            base.OnInit(e);
+        }
+    }
+}
