dbc879e10f2a0bc60ca5112a173336e2a5c8a44c
David Betz <dfb@davidbetz.net>
2015-04-23 13:15:40 -0500

    add metastring

---
+++ Core/Nalarium/MetaString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Nalarium
+{
+    [DebuggerDisplay("{Guid}, {String}")]
+    public class MetaString
+    {
+        public string Guid { get; set; }
+
+        public string String { get; set; }
+
+        public Map PropertyData { get; set; }
+
+        public MetaString()
+        {
+            PropertyData = new Map();
+        }
+
+        public static MetaString Create(string @string)
+        {
+            return new MetaString()
+            {
+                Guid = GuidCreator.GetNewGuid(),
+                String = @string
+            };
+        }
+
+        public static MetaString Create(string guid, string @string)
+        {
+            return new MetaString()
+            {
+                Guid = guid,
+                String = @string
+            };
+        }
+
+        public static MetaString Create(string guid, string @string, Map map)
+        {
+            return new MetaString()
+            {
+                Guid = guid,
+                String = @string,
+                PropertyData = map
+            };
+        }
+    }
+}
