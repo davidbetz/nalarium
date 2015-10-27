1db0e906d73f0f93c7d7557817156f22b9a7e686
David Betz <dfb@davidbetz.net>
2015-10-26 23:14:49 -0500

    added luid

---
+++ Core/Nalarium/LuidCreator.cs
@@ -0,0 +1,42 @@
+#region Copyright
+
+//+ Copyright © Jampad Technology, Inc. 2007-2015
+
+#endregion
+
+using System;
+using System.Text;
+
+namespace Nalarium
+{
+    /// <summary>
+    /// Creates locally unique identifiers
+    /// </summary>
+    public static class LuidCreator
+    {
+        //- @GetNewLuid -//
+        /// <summary>
+        /// Gets a new locally unique identifier as a string
+        /// </summary>
+        /// <value>The new locally unique identifier as a string</value>
+        public static string GetNewLuid()
+        {
+            return GetLuid().Replace("{", "").Replace("}", "");
+        }
+
+        //- @GetNewLuidByteArray -//
+        /// <summary>
+        /// Gets a new locally unique identifier as a byte array
+        /// </summary>
+        /// <value>The new locally unique identifier as a byte array</value>
+        public static byte[] GetNewLuidByteArray()
+        {
+            return Encoding.UTF8.GetBytes(GetLuid());
+        }
+
+        private static string GetLuid()
+        {
+            return Guid.NewGuid().ToString().Replace("{", "").Split('-')[0];
+        }
+    }
+}
