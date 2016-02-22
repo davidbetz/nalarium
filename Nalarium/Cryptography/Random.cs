be27dcdf351a480f7594b8daa0eeabe7fc701eef
David Betz <dfb@davidbetz.net>
2016-02-22 12:23:47 -0600

    fix int64 bug

---
+++ Nalarium/Cryptography/Random.cs
@@ -0,0 +1,29 @@
+#region Copyright
+
+//+ Copyright © David Betz 2007-2016
+
+#endregion
+
+using System.Security.Cryptography;
+
+namespace Nalarium.Cryptography
+{
+    public static class Random
+    {
+        public static long Create(int size)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var rn = new byte[size];
+                rng.GetBytes(rn);
+                if(size < 8)
+                {
+                    var temp = new byte[8];
+                    rn.CopyTo(temp, 0);
+                    rn = temp;
+                }
+                return System.BitConverter.ToInt64(rn, 0);
+            }
+        }
+    }
+}
