66fc95def649f82b3eef81868177bcf0243f0e67
David Betz <dfb@davidbetz.net>
2016-04-23 22:58:48 -0500

    add unit test

---
+++ Nalarium.Test/Binary.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Nalarium.Test
+{
+    [TestClass]
+    public class Binary
+    {
+        [TestMethod]
+        public void Decompose()
+        {
+            var results = Nalarium.Binary.Decompose(12345);
+
+            var expected = new List<long>(new long[] { 1, 8, 16, 32, 4096, 8192 });
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
