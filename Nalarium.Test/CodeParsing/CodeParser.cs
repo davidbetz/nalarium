c711cedd410bb2b5c846b72b5a273bf28142a685
David Betz <dfb@davidbetz.net>
2016-04-26 21:22:48 -0500

    redo codeparser

---
+++ Nalarium.Test/CodeParsing/CodeParser.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nalarium.Test
+{
+    public class AmazonAffiliateCodeParser : Nalarium.CodeParsing.CodeParserTemplate
+    {
+        public override string Code => "AmazonAffiliate";
+
+        public override string Template => @"<a href=""http://www.amazon.com/gp/product/{ASIN}/{AffiliateCode}"">{Title}</a>";
+    }
+
+    public class YouTubeCodeParser : Nalarium.CodeParsing.CodeParserTemplate
+    {
+        public override string Code => "YouTube";
+
+        public override string Template => @"https://www.youtube.com/watch?v={Code}";
+    }
+
+    [TestClass]
+    public class CodeParser
+    {
+        [TestMethod]
+        public void ParseCode_AmazonAffiliate()
+        {
+            var series = new Nalarium.CodeParsing.CodeParser();
+            series.Add(new AmazonAffiliateCodeParser().AddCode("AffiliateCode", "my-amazon-code"));
+
+            var input = "The book you should study is {{AmazonAffiliate:ASIN=B00SLXVBC4;Title=Elasticsearch: The Definitive Guide}}.";
+
+            var results = series.ParseCode(input);
+
+            var expected = @"The book you should study is <a href=""http://www.amazon.com/gp/product/B00SLXVBC4/my-amazon-code"">Elasticsearch: The Definitive Guide</a>.";
+
+            Assert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void ParseCode_YouTube()
+        {
+            var series = new Nalarium.CodeParsing.CodeParser();
+            series.Add(new YouTubeCodeParser());
+
+            var input = "The book is {{YouTube:Code=XC2RYiaM6WU}}.";
+
+            var results = series.ParseCode(input);
+
+            var expected = "The book is https://www.youtube.com/watch?v=XC2RYiaM6WU.";
+
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ Nalarium/CodeParsing/CodeParserTemplate.cs
@@ -0,0 +1,47 @@
+﻿#region Copyright
+
+//+ Copyright © David Betz 2007-2015
+
+#endregion
+
+using System.Collections.Generic;
+
+namespace Nalarium.CodeParsing
+{
+    public abstract class CodeParserTemplate
+    {
+        public abstract string Code { get; }
+
+        public abstract string Template { get; }
+
+        public CodeParserTemplate AddCode(string key, string value)
+        {
+            if (CodeData == null)
+            {
+                CodeData = new Dictionary<string, string>();
+            }
+            CodeData.Add(key, value);
+
+            return this;
+        }
+
+        public void MergeMap(Map map)
+        {
+            if (CodeData == null)
+            {
+                return;
+            }
+            foreach (var key in CodeData.Keys)
+            {
+                map.Add(key, CodeData[key]);
+            }
+        }
+
+        public Dictionary<string, string> CodeData { get; set; }
+
+        public virtual Map Setup()
+        {
+            return new Map();
+        }
+    }
+}
